6c9b91271da79820831c39bba48fc31764d26f2e
Smolarz <mjsz@gft.com>
2018-07-20 16:14:05 +0200

    Solution for CycliRotation

--- src/main/java/arrays/CyclicRotation.java
+++ src/main/java/arrays/CyclicRotation.java
@@ -1,9 +1,24 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class CyclicRotation {
 
     public int[] solution(int[] integers, int rotations) {
 
-        return null;
+        rotations = rotations % integers.length;
+        if (rotations == 0) return integers;
+
+        int splitPoint = integers.length - rotations;
+        int[] subarrayFirst = Arrays.copyOfRange(integers, 0, splitPoint);
+        int[] subarraySecond = Arrays.copyOfRange(integers, splitPoint, integers.length);
+
+        return concatArrays(subarraySecond, subarrayFirst);
+    }
+
+    private int[] concatArrays(int[] first, int[] second) {
+        int[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
     }
 }
--- src/test/java/arrays/CyclicRotationTest.java
+++ src/test/java/arrays/CyclicRotationTest.java
@@ -2,20 +2,34 @@ package arrays;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class CyclicRotationTest {
 
     @Test
     public void testFirstCase() throws Exception {
-        assertEquals(new int[]{9, 7, 6, 3, 8}, new CyclicRotation().solution(new int[]{3, 8, 9, 7, 6}, 3));
+        assertArrayEquals(new int[]{9, 7, 6, 3, 8}, new CyclicRotation().solution(new int[]{3, 8, 9, 7, 6}, 3));
     }
-
-
 
     @Test
     public void testSecondCase() throws Exception {
-        assertEquals(new int[]{1,2,3,4}, new CyclicRotation().solution(new int[]{1,2,3,4}, 4));
+        assertArrayEquals(new int[]{6, 3, 8, 9, 7}, new CyclicRotation().solution(new int[]{3, 8, 9, 7, 6}, 1));
+    }
+
+    @Test
+    public void testRotationsBiggerThanArray() throws Exception {
+        assertArrayEquals(new int[]{3, 4, 1, 2}, new CyclicRotation().solution(new int[]{1, 2, 3, 4}, 10));
+    }
+
+
+    @Test
+    public void testShouldReturnSameArray() throws Exception {
+        assertArrayEquals(new int[]{1, 2, 3, 4}, new CyclicRotation().solution(new int[]{1, 2, 3, 4}, 4));
+    }
+
+    @Test
+    public void testShouldReturnSameArraySecondCase() throws Exception {
+        assertArrayEquals(new int[]{1, 2, 3, 4}, new CyclicRotation().solution(new int[]{1, 2, 3, 4}, 8));
     }
 
 }
