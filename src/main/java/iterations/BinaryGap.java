9e265f9dc255a4c933916dbb38460d2e8a18b5a5
Smolarz <mjsz@gft.com>
2018-07-20 15:14:43 +0200

    Implementation of BinaryGap - iteration

--- src/main/java/iterations/BinaryGap.java
+++ src/main/java/iterations/BinaryGap.java
@@ -2,9 +2,20 @@ package iterations;
 
 public class BinaryGap {
 
-    public int solution(int N) {
-
-
-        return 0;
+    public int solution(int number) {
+        String binary = Integer.toBinaryString(number);
+        int maxGap = 0;
+        int gapCounter = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '0'){
+                gapCounter++;
+            } else {
+                if (gapCounter > maxGap){
+                    maxGap = gapCounter;
+                }
+                gapCounter = 0;
+            }
+        }
+        return maxGap;
     }
 }
