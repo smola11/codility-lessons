7e188011e07d53fc058c368671684cecf8bc4c28
Smolarz <mjsz@gft.com>
2018-07-25 08:26:40 +0200

    Added PermMissingElem solution and tests

--- src/main/java/timeComplexity/PermMissingElem.java
+++ src/main/java/timeComplexity/PermMissingElem.java
@@ -1,10 +1,39 @@
 package timeComplexity;
 
+import java.util.Arrays;
+
 public class PermMissingElem {
 
-    public int solution(int [] integers, int differentInts) {
+    public int solution(int[] integers) {
 
-        return 0;
+        // if array has 0 or 1 element
+        if (integers.length == 0) return 1;
+        if (integers.length == 1) {
+            if (integers[0] == 1) return 2;
+            else return 1;
+        }
+        Arrays.sort(integers);
+        for (int i = 0; i < integers.length - 1; i++) {
+            if (integers[i] + 1 != integers[i + 1]) {
+                return integers[i] + 1;
+            }
+        }
+        // if last or first missing
+        if (integers[integers.length - 1] == integers.length + 1) {
+            return integers[0] - 1;
+        } else {
+            return integers[integers.length - 1] + 1;
+        }
+    }
+
+    // solution using more "mathematical" approach
+    public int solutionWithMathApproach(int[] integers) {
+        long N = integers.length + 1;
+        long total = N * (N + 1) / 2;
+        for (int i : integers) {
+            total -= i;
+        }
+        return (int) total;
     }
 
 
--- src/test/java/timeComplexity/PermMissingElemTest.java
+++ src/test/java/timeComplexity/PermMissingElemTest.java
@@ -8,7 +8,32 @@ class PermMissingElemTest {
 
     @Test
     public void testFirstCase() throws Exception {
-        assertEquals(3, new PermMissingElem().solution(new int[]{2,3,1,5}, 4));
+        assertEquals(4, new PermMissingElem().solution(new int[]{2,3,1,5}));
+    }
+
+    @Test
+    public void testMissingLast() throws Exception {
+        assertEquals(5, new PermMissingElem().solution(new int[]{2,3,1,4}));
+    }
+
+    @Test
+    public void testMissingFirst() throws Exception {
+        assertEquals(1, new PermMissingElem().solution(new int[]{2,3,5,4}));
+    }
+
+    @Test
+    public void testEmptyArray() throws Exception {
+        assertEquals(1, new PermMissingElem().solution(new int[]{}));
+    }
+
+    @Test
+    public void testOneElementArrayWithOne() throws Exception {
+        assertEquals(2, new PermMissingElem().solution(new int[]{1}));
+    }
+
+    @Test
+    public void testOneElementArrayWithTwo() throws Exception {
+        assertEquals(1, new PermMissingElem().solution(new int[]{2}));
     }
 
 }
